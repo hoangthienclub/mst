3c29b3b95f2dea5cc5ef4ee697fad3f9a048671c
Khanh <khanh.nguyen@saigonvalley.com>
2020-06-05 11:27:50 +0700

    remove import

--- src/dbFunctions/index.js
+++ src/dbFunctions/index.js
@@ -1,10 +1,8 @@
 import getPosts from './post/getPosts';
 import getPost from './post/getPost';
-import getClasses from './class/getClasses';
 
 const db = {
   getPosts,
   getPost,
-  getClasses,
 };
 export default db;
