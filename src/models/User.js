d9f806701d147a953a3cdfb0e7950118a93ac735
Thien Tran <thien.tran@saigonvalley.com>
2020-06-08 14:52:44 +0700

    add default thumbnail

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,10 @@ const UserSchema = new mongoose.Schema(
     gender: String,
     status: Number, //1: wait active, 2: active
     password: String,
-    thumbnail: String,
+    thumbnail: {
+      default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQ3XRs4sh5V-q98Pi6ijQtuSE_qP22pLC9FMUUdKvEIgQjhEy2&usqp=CAU',
+      type: String
+    },
     avatar: String,
   },
   { timestamps: true, versionKey: false }
