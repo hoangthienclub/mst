b10180e4ca2c2fbcbf8511d8cc555319ec0b24c5
Thien Tran <thien.tran@saigonvalley.com>
2020-06-12 16:48:41 +0700

    search users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -14,6 +14,10 @@ export const getUsers = async (req, res, next) => {
       query.email = req.query.email;
     }
 
+    if (req.query.search) {
+      query.email = { $regex: new RegExp(req.query.text, 'i') }
+    }
+
     const totalUser = await User.count(query);
     const users = await User.find(query).limit(+pageSize).skip((+page - 1) * +pageSize);
     return Success(res, {
