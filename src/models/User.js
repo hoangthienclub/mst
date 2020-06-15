67d8b546abe843664ec903d6a952a22b4742fb80
Thien Tran <thien.tran@saigonvalley.com>
2020-06-15 16:22:10 +0700

    default user

--- src/models/User.js
+++ src/models/User.js
@@ -2,14 +2,26 @@ import mongoose from '../config/database';
 const UserSchema = new mongoose.Schema(
   {
     email: String,
-    firstName: String,
-    lastName: String,
-    roleId: Number,
+    firstName: {
+      type: String,
+      default: 'First Name'
+    },
+    lastName: {
+      type: String,
+      default: 'Last Name'
+    },
+    roleId: {
+      type: Number,
+      default: 1
+    },
     phone: String,
     address: String,
     country: String,
     gender: String,
-    status: Number, //1: wait active, 2: active
+    status: {
+      type: Number,
+      default: 1
+    }, //1: wait active, 2: active
     password: String,
     thumbnail: {
       default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQ3XRs4sh5V-q98Pi6ijQtuSE_qP22pLC9FMUUdKvEIgQjhEy2&usqp=CAU',
